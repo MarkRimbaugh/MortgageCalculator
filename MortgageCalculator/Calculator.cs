07ffb3e0b8caea2994a7f092868bd2e3e077c9e0
Mark Rimbaugh <mark.rimbaugh@outlook.com>
2022-10-14 11:24:27 -0400

    First commit

---
+++ MortgageCalculator/Calculator.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator
+{
+    public class MortgageCalculator
+    {
+        public double MonthlyIncome { get; set; }
+        public double MarketValue { get; set; }
+        public double PurchasePrice { get; set; }
+        public double HOA_Fee { get; set; }
+        public double DownPayment { get; set; }
+        public int PaymentsPerYear { get; set; }
+        public double LoanAmount { set; get; }
+        public double InterestRate { get; set; }
+        public int Term { get; set; }
+
+        const double TAXES_AND_CLOSING_CLOSTS = 2500;
+        const double PROPERTY_TAX = .0125;
+        const double HOMEOWNERS_INSURANCE = .0075;
+        const double INCOME_THRESHOLD = .25;
+
+        public MortgageCalculator() { }
+
+        public MortgageCalculator(double monthlyIncome, double marketValue, double purchasePrice, double hoaFee, double downPayment, int paymentsPerYear, double interestRate, int term)
+        {
+            MonthlyIncome = monthlyIncome;
+            MarketValue = marketValue;
+            PaymentsPerYear = paymentsPerYear;
+            HOA_Fee = hoaFee;
+            DownPayment = downPayment;
+            InterestRate = interestRate;
+            Term = term;
+            PurchasePrice = purchasePrice;
+        }
+
+        public double CalculateTotalLoanValue()
+        {
+            double loanAmount = (PurchasePrice - DownPayment);
+            double originationFee = loanAmount * .01;
+            double totalLoanValue = loanAmount + originationFee + TAXES_AND_CLOSING_CLOSTS;
+
+            return Math.Round(totalLoanValue,2);
+        }
+
+        public double CalculateBaseLoanAmount()
+        {
+            return Math.Round(PurchasePrice - DownPayment);
+        }
+
+        public double CalculateBasePayment()
+        {
+            double monthlyPayment = ((InterestRate / PaymentsPerYear) * CalculateTotalLoanValue()) / (1 - Math.Pow(1 + (InterestRate / PaymentsPerYear), (Term * PaymentsPerYear) * -1));
+             
+            return Math.Round(monthlyPayment,2);
+        }
+
+        public double CalculateTotalMonthlyPayment()
+        {
+            double basePayment = CalculateBasePayment();
+            double hoa = CalculateHOAPayment();
+            double escrow = CalculateEscrowPayment();
+            double insurance = 0;
+            if (RequiresLoanInsurance())
+            {
+                insurance = CalculateLoanInsurancePayment();
+            }
+
+            return Math.Round((basePayment + insurance + hoa + escrow),2);
+        }
+
+        public double CalculateTotalLoanAmount()
+        {
+            double baseAmount = CalculateBaseLoanAmount();
+            double origination = CalculateOriginationFee();
+
+            return Math.Round((baseAmount + origination + TAXES_AND_CLOSING_CLOSTS),2);
+        }
+
+        public double CalculateOriginationFee()
+        {
+            return Math.Round(((PurchasePrice - DownPayment) * .01),2);
+        }
+
+        public double CalculateEquityValue()
+        {
+            double equity = MarketValue - (CalculateTotalLoanValue());
+            return Math.Round(equity,2);
+        }
+
+        public double CalculateEquityPercentage()
+        {
+            double equityPercentage = Math.Round((CalculateEquityValue() / MarketValue) * 100, 2);
+            return equityPercentage;
+        }
+        
+        public bool RequiresLoanInsurance()
+        {
+            if (CalculateEquityValue() < (.1 * CalculateTotalLoanAmount()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double CalculateLoanInsurancePayment()
+        {
+            if (RequiresLoanInsurance())
+            {
+                return Math.Round(((CalculateTotalLoanValue() * .01) / PaymentsPerYear),2);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double CalculateHOAPayment()
+        {
+            return Math.Round(HOA_Fee / 12,2);
+        }
+
+
+        public double CalculateEscrowPayment()
+        {
+            double homeownersInsurance = MarketValue * HOMEOWNERS_INSURANCE;
+            double propertyTaxes = MarketValue * PROPERTY_TAX;
+            double escrowPayment = (homeownersInsurance + propertyTaxes) / (12);
+
+            return Math.Round(escrowPayment,2);
+        }
+
+        public string ApprovalRecommendation()
+        {
+            double incomeThreshold = MonthlyIncome * INCOME_THRESHOLD;
+            
+
+            if(CalculateTotalMonthlyPayment() <= incomeThreshold)
+            {
+                return "Approve!";
+            }
+            else
+            {
+                return $"Denied - monthly payment exceeds {INCOME_THRESHOLD * 100}% of total monthly income.";
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = $"\n***** RESULTS *****\nMonthly Income: ${MonthlyIncome}\nPurchase Price: ${PurchasePrice}\nMarket Value: ${MarketValue}\n" +
+                $"Down Payment: ${DownPayment}\nInterest Rate %: {InterestRate * 100}\nNumber of Payments per Year: {PaymentsPerYear}\n" +
+                $"Loan term (years): {Term}\nHOA Fees: ${HOA_Fee}\nLoan Base Amount: ${CalculateBaseLoanAmount()}\n" +
+                $"Origination Fee: ${CalculateOriginationFee()}\nClosing Costs: ${TAXES_AND_CLOSING_CLOSTS}\n\n" +
+                $"Total: ${CalculateTotalLoanAmount()}\n\nEquity Percent: {CalculateEquityPercentage()}%\nEquity Value: ${CalculateEquityValue()}\n\n" +
+                $"Recommendation: {ApprovalRecommendation()}\n\nBase monthly: ${CalculateBasePayment()}\nInsurance: ${CalculateLoanInsurancePayment()}\n" +
+                $"HOA Fees: ${CalculateHOAPayment()}\nTaxes and Escrow: ${CalculateEscrowPayment()}\n\nTotal monthly payment: ${CalculateTotalMonthlyPayment()}";
+            return output;
+        }
+    }
+}
---
+++ MortgageCalculator/Program.cs
@@ -0,0 +1,197 @@
+﻿using System.Xml.Serialization;
+
+namespace MortgageCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runAgain = true;
+
+            while (runAgain)
+            {
+                /*var monthlyIncome = 10416.66;
+                var marketValue = 356500;
+                var purchasePrice = 375000;
+                var hoaFee = 500;
+                var downPayment = 100000;
+                var monthlyPayments = 12;
+                var interest = 5.75/100;
+                var numberOfYears = 30;*/
+
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine("Welcome to the Mortage Approval Tool");
+            Console.WriteLine(new string('*', 50));
+
+                var monthlyIncome = GetValidDouble("Enter total monthly income: ");
+                var purchasePrice = GetValidDouble("Enter the purchase price for the house: ");
+                var marketValue = GetValidDouble("Enter the market value for the house: ");
+                double downPayment = GetValidDouble("Enter the down payment for the loan: ");
+                var interest = GetValidDouble("Enter the interest rate for the loan: ") / 100;
+                var monthlyPayments = GetValidInt("Enter number of monthly payments: ");
+                var numberOfYears = GetValidInt("Enter term in years [15 or 30] ");
+                var hoaFee = GetValidDouble("Enter annual HOA fee: ");
+
+                var calc1 = new MortgageCalculator(monthlyIncome, marketValue, purchasePrice, hoaFee, downPayment, monthlyPayments, interest, numberOfYears);
+                var approval = calc1.ApprovalRecommendation();
+
+                Console.WriteLine(calc1.ToString());
+
+                runAgain = RunAgain();
+            }
+
+            
+        }
+
+        static bool RunAgain()
+        {
+            bool isValidInput = false;
+            bool runAgain = false;
+
+            while (!isValidInput)
+            {
+                Console.Write("Do you want to run the numbers again? Y/N -> ");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower().StartsWith('y'))
+                {
+                    isValidInput = true;
+                    runAgain = true;
+                }
+                else if (choice.ToLower().StartsWith('n'))
+                {
+                    Console.WriteLine("Goodbye!");
+                    isValidInput = true;
+                    runAgain = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again");
+                }
+
+            }
+            return runAgain;
+        }
+
+        static int GetValidInt(string message)
+        {
+            bool isValid = false;
+            int number = 0;
+            string input;
+
+            while (!isValid)
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+
+                isValid = Int32.TryParse(input, out number);
+
+                if (isValid)
+                {
+                    if(ConfirmInput(number) == false)
+                    {
+                        isValid = false;
+                    }    
+                }
+                if (!isValid)
+                {
+                    Console.WriteLine("Please make another choice.");
+                }
+            }
+            return number;
+        }
+
+        static double GetValidDouble(string message)
+        {
+            bool isValid = false;
+            double number = 0;
+            string input;
+
+            while (!isValid)
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+
+                isValid = Double.TryParse(input, out number);
+
+                if (isValid)
+                {
+                    if (ConfirmDoubleInput(number) == false)
+                    {
+                        isValid = false;
+                    }
+                }
+                if (!isValid)
+                {
+                    Console.WriteLine("Please make another choice.");
+                }
+            }
+            return number;
+        }
+        static bool ConfirmDoubleInput(double number)
+        {
+            string input;
+            bool confirmed = false;
+            bool validChoice = false;
+
+            while (!validChoice)
+            {
+                Console.Write($"You entered {number}. Is that correct? Y/N -> ");
+                input = Console.ReadLine();
+
+                if (input.ToLower().StartsWith('y'))
+                {
+                    validChoice = true;
+                    confirmed = true;
+                }
+                else if (input.ToLower().StartsWith('n'))
+                {
+                    validChoice = true;
+                    confirmed = false;
+                }
+                else
+                {
+                    validChoice = false;
+                    Console.WriteLine("Invalid entry, please choose Y or N");
+                }
+            }
+            return confirmed;
+        }
+
+        static bool ConfirmInput(int number)
+        {
+            string input;
+            bool confirmed = false;
+            bool validChoice = false;
+
+            while (!validChoice)
+            {
+                Console.Write($"You entered {number}. Is that correct? Y/N -> ");
+                input = Console.ReadLine();
+
+                if (input.ToLower().StartsWith('y'))
+                {
+                    validChoice = true;
+                    confirmed = true;
+                }
+                else if (input.ToLower().StartsWith('n'))
+                {
+                    validChoice = true;
+                    confirmed = false;
+                }
+                else
+                {
+                    validChoice = false;
+                    Console.WriteLine("Invalid entry, please choose Y or N");
+                }
+            }
+            return confirmed;
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("1. Create a new loan");
+            Console.WriteLine("");
+        }
+    }
+}
